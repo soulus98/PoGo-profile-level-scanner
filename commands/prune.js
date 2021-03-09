a431de156147ec5a48d508b66a444f382702634e
Soul-Home <soulus98@hotmail.com>
2021-03-09 11:07:14 +1100

    Trying to figure out collections (oops forgot one)

--- commands/prune.js
+++ commands/prune.js
@@ -15,3 +15,17 @@ module.exports = {
     message.channel.bulkDelete(amount);
 	},
 };
+
+
+/*else if (command === "prune") {
+    let replyMessage = message.reply("All messages in this channel will be deleted. Type "yes" to confirm. This will last 10 seconds.");
+    let filter = msg => msg.author.id == message.author.id && msg.content.toLowerCase() == "yes";
+    message.channel.awaitMessages(filter, {max: 1, time: 20000}).then(collected => {
+    if (!replyMessage.deleted) replyMessage.delete();
+    if (!collected.first().deteled) collected.first().delete();
+    message.channel.bulkDelete(10)
+    .then(messages => message.channel.send(`Pruned ${messages.size} messages`))
+    .catch(console.error);
+    message.reply("Confirmed!");
+    });
+  }*/
