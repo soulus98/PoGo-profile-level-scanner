ad165f5043e1f84b5fec7e4cd9f83fd2bbbfcf40
Soul <soulus98@hotmail.com>
2021-04-09 18:19:59 +1000

    First successful scan.

--- bot.js
+++ bot.js
@@ -14,7 +14,6 @@ var screensFolder = `./screens/Auto/${postDate.toDateString()}`;
 config = {};
 module.exports = {loadConfigs};
 
-
 function loadConfigs(){
 	config = {};
 	delete require.cache[require.resolve("./config.json")];
@@ -66,6 +65,7 @@ function load(){
 load();
 client.once("ready", () => {
 	channel = client.channels.cache.get(screenshotChannel);
+	client.user.setActivity(`Use ${prefix}help for commands.`);
 	if (channel == undefined){
 		console.log("Oops the screenshot channel is broken.");
 	};
@@ -98,11 +98,19 @@ client.on("message", message => {
 		if (channel == undefined){
 			message.channel.send(`The screenshot channel could not be found. Please set it correctly using \`${prefix}set screenshotChannel <id>\``);
 		};
-		if (message.channel != channel) return;
+		if (message.channel.type === "dm") {
+			message.reply(`I cannot scan an image in a dm. Please send it in ${channel}`);
+			return;
+		}
+		if (message.channel != channel) {
+			message.reply(`I cannot scan an image in this channel. Please send it in ${channel}.
+<@&${modRole}>, perhaps you should prohibit my access from this (and all other) channels bar ${channel}.`);
+			return;
+		}
 		imageAttempts++;
 		var instance = imageAttempts;
-		console.log(`instance: ${instance}\ncurrentTime: ${currentTime}\nimageLogCount: ${imageLogCount}\nlastImageTimestamp + timeDelay: ${lastImageTimestamp+timeDelay}`);
-		console.log("Compared seconds: " + (postedTime-lastImageTimestamp)/1000);
+		//console.log(`instance: ${instance}\ncurrentTime: ${currentTime}\nimageLogCount: ${imageLogCount}\nlastImageTimestamp + timeDelay: ${lastImageTimestamp+timeDelay}`);
+		//console.log("Compared seconds: " + (postedTime-lastImageTimestamp)/1000);
 		if (lastImageTimestamp+timeDelay<currentTime && instance-1==imageLogCount){
 			imageWrite(message);
 		} else {
@@ -156,80 +164,119 @@ client.on("message", message => {
 			}
 		}
 		async function crop(){
-
+			/*
 			const imgCanv = Canvas.createCanvas(image.width,image.height);	//size of canvas
 			const ctx = imgCanv.getContext("2d");
 			const img = Canvas.loadImage(image.url);
 			img.then((img) => {
 				ctx.drawImage(img,0,0,imgCanv.width,imgCanv.height);
+			*/
 
-			/*const imgCanv = Canvas.createCanvas((image.width/7),(image.height/5));	//size of canvas
+			/*const imgCanv = Canvas.createCanvas((image.width/3),(image.height/5));	//size of canvas
 			const ctx = imgCanv.getContext("2d");
 			const img = Canvas.loadImage(image.url);
 			img.then((img) => {
 				ctx.drawImage(img, 	//img (source image)
 					image.width/28,image.height/2.2,	//sx, sy (where to start clipping)
-					imgCanv.width,imgCanv.height,		//swidth, sheight (clip size)
+					imgCanv.width/2,imgCanv.height,		//swidth, sheight (clip size)
 					0,0,														//x, y (where to draw in the Canvas)
 					imgCanv.width,imgCanv.height		//width, height (size of canvas again)
 				);
 				*/
+
+				/*
 				const imgData = ctx.getImageData(0, 0, imgCanv.width, imgCanv.height);
 				newData = greyScale(imgData);
 				ctx.putImageData(newData,0,0);
 				try{
 	  			const imgAttach = new Discord.MessageAttachment(imgCanv.toBuffer(), image.url);
 	  			message.channel.send("Reeeeee", imgAttach);
+
 	      } catch (err){
 	        console.log(`There was an error:  ${err}`);
 	      }
 				//.catch(err => console.log(`An error occured. Error: ${err}`));
-		});
-
-			function greyScale(imgData){
-				for (i = 0; i < imgData.data.length; i += 4) {
-					let count = imgData.data[i] + imgData.data[i + 1] + imgData.data[i + 2];
-					let colour = 0;
-					if (count > 650) colour = 255;
-					imgData.data[i] = colour;
-					imgData.data[i + 1] = colour;
-					imgData.data[i + 2] = colour;
-					imgData.data[i + 3] = 255;
-				}
-				return imgData;
+			});
+			*/
+			recog(image);
+		}
+		function greyScale(imgData){
+			for (i = 0; i < imgData.data.length; i += 4) {
+				let count = imgData.data[i] + imgData.data[i + 1] + imgData.data[i + 2];
+				let colour = 0;
+				if (count > 650) colour = 255;
+				imgData.data[i] = colour;
+				imgData.data[i + 1] = colour;
+				imgData.data[i + 2] = colour;
+				imgData.data[i + 3] = 255;
 			}
-
+			return imgData;
+		}
+		async function recog(image){
+			ratio = image.height/image.width;
+			rectangle = rectGen(image, ratio);
 			try{
 				const worker = createWorker({
-					logger: m => console.log(m),
+				  logger: m => console.log(m)
 				});
 				(async () => {
 					await worker.load();
 					await worker.loadLanguage('eng');
 					await worker.initialize('eng');
-					const { data: { text } } = await worker.recognize(imgCanv.toBuffer());
+					const { data: { text } } = await worker.recognize(screensFolder + "/" + imageName, { rectangle });
+					console.log("Image recognised. Result:");
 					console.log(text);
 					await worker.terminate();
+					level = text.match(/(\d\d)/)[0];
+					console.log(level);
+					if (isNaN(level) || level >50){
+						message.reply(`<@&${modRole}> There was an issue scanning this image. It may not be a Pokemon Go profile screenshot, be too low quality, or there may be an internal bot issue.`);
+					} else {
+						message.reply("Test. Your level was scanned at " + level);
+						roleGrant(level);
+						if (deleteScreens) {
+							message.delete();
+						}
+					}
 				})();
 			}catch (err){
 				console.log(`An error occured while recognising. Error: ${err}`);
 			}
-			level = 50;
-			if (isNaN(level)){
-				message.reply(`<@&${modRole}> There was an issue scanning this image. It may not be a Pokemon Go profile screenshot, or there may be an internal bot issue.`);
+
+		}
+		function rectGen(image, ratio){
+			if (ratio > 2 && ratio < 2.25) {
+				rectangle = {
+					left: image.width/43.2,
+					top: image.height/1.932,
+					width: image.width/5.4,
+					height: image.height/4.1455
+				};
+			} else {
+				rectangle = {
+					left: 25,
+					top: 1100,
+					width: 200,
+					height: 700
+				};
 			}
-			else{
-				roleGrant(level);
-				if (deleteScreens) {
-					message.delete();
-				}
-			}
-		} //ocr end
+			return rectangle;
+		}
 		function roleGrant(level){
 			const msgtxt = [];
 			try {
-				if (level<30 && message.author){
-					msgtxt.push(`Hey trainer!
+				role30 = message.guild.roles.cache.get(level30Role);
+				role40 = message.guild.roles.cache.get(level40Role);
+				role50 = message.guild.roles.cache.get(level50Role);
+				if (message.member.roles.cache.has(level50Role) && message.member.roles.cache.has(level40Role) && message.member.roles.cache.has(level30Role)){
+					message.author.send("You already have all available roles from screenshot scanning.\nThere is no need to post your screenshot.");
+					return;
+				}
+				if(message.member.roles.cache.has(level30Role)){
+					msgtxt.push("You already have the Level 30 role, so there is no need to post your screenshot");
+				}
+				else if (level<30 && message.author){
+					message.author.send(`Hey trainer!
 Thanks so much for your interest in joining our raid server.
 Unfortunately we have a level requirement of 30 to gain full access, and your screenshot was scanned at ${level}.
 Gaining xp is very easy to do now with friendships, events, lucky eggs and so much more! Please stay and hang out with us here.
@@ -239,6 +286,7 @@ Once you've reached that point, please repost your screenshot.
 In the meantime please join our sister server with this link.
 Hope to raid with you soon! :slight_smile:
 https://discord.gg/tNUXgXC`);
+					message.react("👎");
 					//TODO: blacklist & mention server staff DM
 					return;
 				}
@@ -254,19 +302,17 @@ Have fun raiding. :wave:
 				}
 				if (level>39 && level40Role){
 					message.member.roles.add(message.guild.roles.cache.get(level40Role)).catch(console.error);
-					msgtxt.push(`Also, congratulations on achieving such a high level.
-You have been given roles based on reaching:
-Level 30
-Level 40`);
+					msgtxt.push(`${(message.member.roles.cache.has(level30Role)) ? ", however,":"\nAlso, "} we congratulate you on achieving such a high level.\n
+For this you have been given the Level 40 role`);
 				}
 				if (level>49 && level50Role){
 					message.member.roles.add(message.guild.roles.cache.get(level50Role)).catch(console.error);
-					msgtxt.push(`Level 50`);
+					msgtxt.push(`, and the Level 50 role`);
 				}
 			} catch (e) {
 				console.log(`an error occured. Error: ${e}`);
 			}
-			message.author.send(msgtxt, { split: true });
+			message.author.send(msgtxt.toString(), {split:True});
 		}
 	}
 
