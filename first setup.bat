0a99cd7c6051e0f5c17b49dfdc3ed52658bbac42
Soul <soulus98@hotmail.com>
2021-12-03 18:44:54 +1100

    typo

--- first setup.bat
+++ first setup.bat
@@ -5,8 +5,8 @@ pause
 pause
 call npm install
 mkdir server
-echo {"token":""} > server\keys1.json
-echo {"toggles":{"saveLocalCopy":false,"deleteScreens":true,"welcomeMsg":false,"testMode":true,"performanceMode":false,"blacklistOneOff":false,"tagModOneOff":true,"screenshotScanning":true,"dmScanning":true,"dmMail":true,"attachmentURLs":false,"debugMode":false,"dmAutoReply":false,"respondVerify":true,"dmSymbolDenial":true},"numbers":{"threshold":225,"msgDeleteTime":5,"blacklistTime":1,"targetLevel":30},"chars":{"prefix":"]","prefix2":""},"ids":{"screenshotChannel":"","logsChannel":"","profileChannel":"","targetLevelRole":"","level40Role":"","level50Role":"","verifiedRole":"","modRole":"","serverID":"","blacklistRole":"","mailCategory":"","mailLogChannel":""}} > server\config1.json
+echo {"token":""} > server\keys.json
+echo {"toggles":{"saveLocalCopy":false,"deleteScreens":true,"welcomeMsg":false,"testMode":true,"performanceMode":false,"blacklistOneOff":false,"tagModOneOff":true,"screenshotScanning":true,"dmScanning":true,"dmMail":true,"attachmentURLs":false,"debugMode":false,"dmAutoReply":false,"respondVerify":true,"dmSymbolDenial":true},"numbers":{"threshold":225,"msgDeleteTime":5,"blacklistTime":1,"targetLevel":30},"chars":{"prefix":"]","prefix2":""},"ids":{"screenshotChannel":"","logsChannel":"","profileChannel":"","targetLevelRole":"","level40Role":"","level50Role":"","verifiedRole":"","modRole":"","serverID":"","blacklistRole":"","mailCategory":"","mailLogChannel":""}} > server\config.json
 echo(
 echo(
 echo(
